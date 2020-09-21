c7c6cc434836ca956b212107f10fea09f859af00
Marcin Krztoń-Maziopa <49957034+mkrzton@users.noreply.github.com>
2020-09-21 14:38:18 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-
-import History from '../src/components/History'
-
-import Mail from '../src/components/Mail'
 import './App.css';
+import Home from './containers/Home';
+import Contact from './containers/Contact';
+import Products from './containers/Products';
+import About from './containers/About';
+import Error from './containers/Errror'
+import Single from './containers/Single';
+import {Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <>
-    
-    <History></History>
-    <Mail></Mail>
-   
-    </>
+    <div className="App">
+    <Navbar></Navbar>
+    <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/products" component={Products}/>
+    <Route exact path="/about" component={About}/>
+    <Route exact path="/products/:slug" component={Single}/>
+    <Route exact path="/contact" component={Contact}/>
+    <Route component={Error} />
+    </Switch>
+    <Footer></Footer>
+    </div>
   );
 }
 
---
+++ src/Contentful.js
@@ -0,0 +1,7 @@
+import {createClient} from 'contentful'
+
+export default createClient({
+
+    space: "wf2k2nc85z5m",
+    accessToken: "4Q5l3e22kpz7GiRte1SQ7p-vbApE17dvJGnaKT8jINA"
+});
