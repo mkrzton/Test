9c8c67004cbc49a102785d9ff001d7fcd466f5f9
Marcin Krztoń-Maziopa <49957034+mkrzton@users.noreply.github.com>
2020-09-23 19:55:31 +0200

    Add files via upload

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,85 +1,66 @@
 import React from "react";
-import Banner from '../../components/Banner'
 import {Link} from 'react-router-dom'
-import SmartSlider from "react-smart-slider";
+import Slider2 from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "normalize.css/normalize.css";
+import "./slider-animations.css";
 import "./style.css";
 
-
-const DummyCaption = ({ caption }) => (
-  <div style={{
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-    border: 'solid 1px var(--primaryColor)',
-  }}>
-    {caption}
-  </div>
-)
-
-export default function Slider() {
-  const slidesArray = [
-    {
-        url: "https://i.imgur.com/7u8i7L1.jpg",
-        childrenElem: <DummyCaption caption={ 
-        <Banner title="Preformy i Pojemniki PET"> 
-            <Link to="/products" className="btn-primary">
-                Produkty
-            </Link>
-        </Banner>} />
-      },
-      {
-        url: "https://i.imgur.com/E8gkF2f.jpg",
-        childrenElem: <DummyCaption caption={ 
-        <Banner title="Zamówienia realizowane na całą Europę" > 
-            <Link to="/contact" className="btn-primary">
-               Kontakt 
-            </Link>
-        </Banner>} />
-      },
-      {
-        url: "https://i.imgur.com/t2a1zLi.jpg",
-        childrenElem: <DummyCaption caption={
-          <Banner title="Preformy i Pojemniki PET" > 
-              <Link to="/products" className="btn-primary">
-                Produkty
-              </Link>
-          </Banner>
-        } />
-      },
-  ];
-  return (
-    <div className="around">
-      <SmartSlider
-        slides={slidesArray}
-        buttonShape="round"
-        height={800}
-        autoSlide={true}
-        showIndicators={false}
-        autoSlideInterval={5000}
-      />
+const content = [
+  {
+    title: "Vulputate Mollis  Parturient",
+    description:
+      "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+    button: "Produkty",
+    slug: "/products",
+    transition: true,
+    image: "https://i.imgur.com/ZXBtVw7.jpg"
+  },
+  {
+    title: "Tortor Dapibus Commodo Aenean Quam",
+    description:
+      "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+    button: "Kontakt",
+    slug: './contact',
+    transition: true,
+    image: "https://i.imgur.com/DCdBXcq.jpg"
+  },
+  {
+    title: "Phasellus volutpat metus",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+    transition: false,
+    image: "https://i.imgur.com/DvmN8Hx.jpg"
+  }
+];
+export default function Slider(){
+  
+  
+    return(
+       
+      <div>
+      <Slider2 autoplay="1500"className="slider-wrapper">
+        {content.map((item, index) => (
+          <div
+            key={index}
+            className="slider-content"
+            style={{ background: `url('${item.image}') no-repeat center center` }}
+          >
+            <div className="inner">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              {item.transition ? <>
+                <Link to={item.slug} className="slider-but">
+                                 {item.button}
+                            </Link>
+                        </>
+                        : <></>}
+            
+            </div>
+          </div>
+        ))}
+      </Slider2>
+     
     </div>
-  );
+    )
 }
-
-// MIT License
-
-// Copyright (c) 2019 Rajesh kumar
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
